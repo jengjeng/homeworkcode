0476990ab1612de876a1a4a7c46fe4ec1ef9f3f6
Jeng <kinkongjeng@hotmail.com>
2017-01-29 00:39:40 +0700

    fix resouces loader

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import { Col, Row, Menu, MenuItem, MenuItemGroup, Tabs, TabPane } from 'element-ui'
 import App from './App.vue'
 
-Vue.use(ElementUI)
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Tabs)
+Vue.use(TabPane)
 
 /* eslint-disable no-new */
 new Vue({
